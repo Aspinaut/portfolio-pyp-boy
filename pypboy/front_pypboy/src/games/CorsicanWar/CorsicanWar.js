af12e2812a086c51aa882b88f37a61a367981688
Vincent Masse <vincentmasse@protonmail.com>
2021-06-18 10:20:48 +0200

    create temporary basic game logic

--- pypboy/front_pypboy/src/games/CorsicanWar/CorsicanWar.js
+++ pypboy/front_pypboy/src/games/CorsicanWar/CorsicanWar.js
@@ -1,8 +1,105 @@
+import React, { useState, useEffect } from 'react'
+
+class Card {
+  constructor(suit, value, power) {
+    this.suit = suit
+    this.value = value
+    this.power = power
+  }
+}
+class Deck {
+  constructor() {
+    this.deck = []
+  }
+
+  shuffle() {
+    for (let count=this.deck.length - 1; count >= 0; count--) {
+      let temp = this.deck[count]
+      let rnd = Math.floor(Math.random() * count)
+      this.deck[count] = this.deck[rnd]
+      this.deck[rnd] = temp
+    }
+  }
+
+  createDeck() {
+    for (let suit of suits) {
+      let power = 2
+      for (let value of values) {
+        this.deck.push(new Card(suit, value, power))
+        power++
+      }
+    }
+  }
+}
+
+const suits = ["C", "S", "H", "D"]
+const values = ["2","3","4","5","6","7","8","9","T","J","Q","K","A"]
+let gameReady = false
+let deckP1 = []
+let deckP2 = []
+let cardP1
+let cardP2
+
+function startGame() {
+  console.log("Start Game !")
+  let deck = new Deck()
+  deck.createDeck()
+  deck.shuffle()
+  for (let i=0; i < deck.deck.length; i++) {
+    deckP1.push(deck.deck[i++])
+    deckP2.push(deck.deck[i])
+  }
+  gameReady = true
+  console.log(deckP1.length, deckP2.length)
+}
+
+function gameLoop() {
+  if (!gameReady) return
+  console.log("game is looping...")
+  cardP1 = deckP1.pop()
+  cardP2 = deckP2.pop()
+  console.log("P1 sort un " + cardP1.power + " !")
+  console.log("P2 sort un " + cardP2.power + " !")
+
+  if (cardP1.power > cardP2.power) {
+    console.log("P1 l'emporte !")
+    deckP1.unshift(cardP1)
+    deckP1.unshift(cardP2)
+    console.log(deckP1, deckP2)
+    if (deckP2.length === 0) {
+      console.log("P1 remporte la partie !")
+      gameReady = false
+    }
+  } else if (cardP1.power < cardP2.power) {
+    console.log("P2 l'emporte !")
+    deckP2.unshift(cardP1)
+    deckP2.unshift(cardP2)
+    console.log(deckP1, deckP2)
+    if (deckP1.length === 0) {
+      console.log("P2 remporte la partie !")
+      gameReady = false
+    }
+  } else {
+    deckP1.unshift(cardP1)
+    deckP2.unshift(cardP2)
+    console.log("Egalité !")
+  }
+}
+
+setInterval(() => gameLoop(), 3000)
+
 function CorsicanWar() {
+
+  useEffect(() => {
+
+  })
+
+
   return (
-    <>
+    <div className="text-center mt-5">
       <h1>Corsican War !</h1>
-    </>
+      <button onClick={startGame}>Start Game</button>
+    </div>
   )
 }
 
--- pypboy/front_pypboy/src/games/War/War.js
+++ pypboy/front_pypboy/src/games/War/War.js
@@ -6,13 +6,16 @@ import { Deck, DeckBack } from '../Cards'
 function War() {
   const CANVAS_WIDTH = 1080
   const CANVAS_HEIGHT = 600
-  let deckBack = new DeckBack()
+  const deckBack = new DeckBack()
   deckBack.createDeck()
   const CARD_WIDTH = deckBack.deck[0].image.width
   const CARD_HEIGHT = deckBack.deck[0].image.height
   const canvasRef = useRef()
   const [deckP1, setDeckP1] = useState([])
   const [deckP2, setDeckP2] = useState([])
+  const [cardP1, setCardP1] = useState(null)
+  const [cardP2, setCardP2] = useState(null)
+  const [gameReady, setGameReady] = useState(false)
   const posDeckP1 = {
     x: (CANVAS_WIDTH / 2) - (CARD_WIDTH * 2),
     y: (CANVAS_HEIGHT / 2) - (CARD_HEIGHT / 2)
@@ -32,59 +35,64 @@ function War() {
 
   const deal = (deckToDeal) => {
     for (let i=0; i < deckToDeal.length; i++) {
-      deckP1.push(deckToDeal[i++])
-      deckP2.push(deckToDeal[i])
+      setDeckP1(deckP1.push(deckToDeal[i++]))
+      setDeckP2(deckP2.push(deckToDeal[i]))
     }
   }
 
   const checkGreater = (card1, card2) => {
     if (card1 && card2) {
-      card1 > card2 ? setDeckP1(deckP1.push(card1, card2)) : setDeckP2(deckP2.push(card1, card2))
+      card1.power > card2.power ? setDeckP1(deckP1.unshift(card1, card2)) : setDeckP2(deckP2.unshift(card1, card2))
     }
   }
 
   const startGame = () => {
     console.log("start game !")
-    const context = canvasRef.current.getContext("2d")
     let deck = new Deck()
     deck.createDeck()
-    console.log(deck)
     deck.shuffle()
     deal(deck.deck)
-    context.drawImage(deckBack.deck[4].image, posDeckP1.x, posDeckP1.y)
-    context.drawImage(deckBack.deck[14].image, posDeckP2.x, posDeckP2.y)
-
-    canvasRef.current.addEventListener('click', (mouseEvent) => {
-      const rect = canvasRef.current.getBoundingClientRect()
-      const x = mouseEvent.clientX - rect.left
-      const y = mouseEvent.clientY - rect.top
-      let cardP1
-      let cardP2
-      if (((x > posDeckP1.x && x < posDeckP1.x + CARD_WIDTH)
-      && (y > posDeckP1.y && y < posDeckP1.y + CARD_HEIGHT))
-      || ((x > posDeckP2.x && x < posDeckP2.x + CARD_WIDTH)
-      && (y > posDeckP2.y && y < posDeckP2.y + CARD_HEIGHT))) {
-        cardP1 = deckP1.pop()
-        cardP2 = deckP2.pop()
-        context.drawImage(cardP2.image, posCardP2.x, posCardP2.y)
-        context.drawImage(cardP1.image, posCardP1.x, posCardP1.y)
-        checkGreater(cardP1, cardP2)
-      }
-    })
+    setGameReady(true)
   }
 
   const gameLoop = () => {
+    if (gameReady) {
+      canvasRef.current.addEventListener('click', (mouseEvent) => {
+        const rect = canvasRef.current.getBoundingClientRect()
+        const x = mouseEvent.clientX - rect.left
+        const y = mouseEvent.clientY - rect.top
 
+        if (((x > posDeckP1.x && x < posDeckP1.x + CARD_WIDTH)
+        && (y > posDeckP1.y && y < posDeckP1.y + CARD_HEIGHT))
+        || ((x > posDeckP2.x && x < posDeckP2.x + CARD_WIDTH)
+        && (y > posDeckP2.y && y < posDeckP2.y + CARD_HEIGHT))) {
+          // setCardP1(deckP1.pop())
+          // setCardP2(deckP2.pop())
+          setCardP1(deckP1[deckP1.length - 1])
+          setCardP2(deckP2[deckP2.length - 1])
+          console.log(cardP1, cardP2)
+          console.log("P1 sort un " + cardP1.power + " !")
+          console.log("P2 sort un " + cardP2.power + " !")
+          setInterval(() => checkGreater(cardP1, cardP2), 3000)
+        }
+      })
+    }
   }
 
   useEffect(() => {
     const context = canvasRef.current.getContext("2d")
-    // context.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height)
+    context.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height)
+    context.drawImage(deckBack.deck[4].image, posDeckP1.x, posDeckP1.y)
+    context.drawImage(deckBack.deck[14].image, posDeckP2.x, posDeckP2.y)
+
+    if (cardP1) context.drawImage(cardP1.image, posCardP1.x, posCardP1.y)
+    if (cardP2) context.drawImage(cardP2.image, posCardP2.x, posCardP2.y)
+
     context.fillText(deckP1.length, posDeckP1.x + (CARD_WIDTH / 2), posDeckP1.y)
     context.fillText(deckP2.length, posDeckP2.x + (CARD_WIDTH / 2),  posDeckP2.y)
   })
 
-  // useInterval(() => gameLoop(), 1000)
+  useInterval(() => gameLoop(), 1000)
 
   return (
     <>
