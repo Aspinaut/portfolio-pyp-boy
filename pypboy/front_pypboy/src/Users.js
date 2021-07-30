7c5c8e1aafaf263e8062b2edc50d072b83ce1463
Vincent Masse <vincentmasse@protonmail.com>
2021-07-30 12:27:35 +0200

    fix Users axios

--- pypboy/front_pypboy/src/Users.js
+++ pypboy/front_pypboy/src/Users.js
@@ -1,78 +1,25 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
-// function Users()
-// {
-//     const [users, getUsers] = useState([
-//         {
-//             id: 0,
-//             username: '',
-//             email: ''
-//         }
-//     ])
-
-//     const getUsersList = () => {
-//         let options = {
-//             method: "GET",
-//             url: 'http://localhost:8000/api/users/'   
-//         }
-//         axios(options)
-//         .then(response => {
-//             getUsers([
-//                 ...response.data
-//             ])
-//         })
-//         .catch(error => console.error(error))
-//     }
-
-//     useEffect(() => {
-//         getUsersList()
-//     }, [])
-    
-//     return (
-//         <>
-//            {users[0].username}
-
-//             {<ul className="user-list">
-//                 {users.forEach(user => {
-//                     <li>user.username</li>
-//                 })}
-//             </ul>}
-//         </>
-//     )
-// }
-
-const api = axios.create({
-    baseURL: 'http://localhost:8000/api/'
-})
-
-class Users extends React.Component
+function Users()
 {
-    state = {
-        users: []
-    }
-    constructor() {
-        super()
-        api.get('users/').then(response => {
-            this.setState({users: response.data})
-            console.log(response.data)
-        })
-    }
-    render() {
-        return (
-            <>
-                {this.state.users[0].username}
-
-                {<ul className="user-list">
-                    {this.state.users.map(user => {
-                        return (
-                            <li>user.username</li>
-                        )
-                    })}
-                </ul>}
-            </>
-        )
-    }
+    const [users, setUsers] = useState([])
+    useEffect(() => {
+        axios
+            .get('http://localhost:8000/api/users/')
+            .then(res => {
+                console.log(res.data)
+                setUsers(res.data)
+            })
+            .catch(err => console.log(err))
+    }, [])
+    return (
+        <>
+            <ul className="user-list">
+                {users.map(user => <li>{user.username}</li>)}
+            </ul>
+        </>
+    )
 }
 
-export default Users;
+export default Users
