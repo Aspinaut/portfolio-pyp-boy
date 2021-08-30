d09447128a30ebd765f6e1b143d28cebc7fe4dd2
Vincent Masse <vincentmasse@protonmail.com>
2021-08-30 17:53:06 +0200

    add heroku-django

--- pypboy/pypboy/settings.py
+++ pypboy/pypboy/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/3.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
-
+import django_heroku
+django_heroku.settings(locals())
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
